bb9e0c1be508b445d332f5e6e502679b4fd8b8a0
ismailbalpetek-cyber <ismailbalpetek@gmail.com>
2025-12-31 02:35:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,27 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 async def mesaj_yakala(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        f"✅ Mesaj alındı\n\n{update.message.text}"
-    )
+
+    if update.message.text:
+        await update.message.reply_text(
+            "📝 Metin alındı.\n\nBir sonraki aşamada bu sorudan yeni sorular üreteceğim."
+        )
+
+    elif update.message.photo:
+        await update.message.reply_text(
+            "🖼️ Görsel alındı.\n\nBir sonraki aşamada resimden soruyu okuyacağım."
+        )
+
+    elif update.message.document:
+        await update.message.reply_text(
+            "📄 Dosya alındı.\n\nBir sonraki aşamada içeriği analiz edeceğim."
+        )
+
+    else:
+        await update.message.reply_text(
+            "⚠️ Desteklenmeyen içerik."
+        )
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, mesaj_yakala))
-
+app.add_handler(MessageHandler(filters.ALL, mesaj_yakala))
 app.run_polling()
