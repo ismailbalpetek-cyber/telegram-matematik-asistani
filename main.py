5c58f481f5a63126732c39a07fd05c0210887256
ismailbalpetek-cyber <ismailbalpetek@gmail.com>
2025-12-31 02:31:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import os
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 async def mesaj_yakala(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        f"Mesaj alındı ✅\n\n{update.message.text}"
+        f"✅ Mesaj alındı\n\n{update.message.text}"
     )
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
