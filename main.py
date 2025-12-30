53f59f10fcd0f6462f5c055b7fd4ecd7aab6ec1a
ismailbalpetek-cyber <ismailbalpetek@gmail.com>
2025-12-31 02:01:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
+import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    MessageHandler,
+    filters
+)
+from flask import Flask, request
 
-BOT_TOKEN = "8545319991:AAGMxJ4L_iDZgdYu3yLaCGpncNiHpj7ipRE"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+app_flask = Flask(__name__)
 
 async def mesaj_yakala(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    gelen_mesaj = update.message.text
     await update.message.reply_text(
-        f"Mesaj alındı ✅\n\nGönderdiğin içerik:\n{gelen_mesaj}"
+        f"Mesaj alındı ✅\n\nGönderdiğin içerik:\n{update.message.text}"
     )
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+@app_flask.route(f"/{BOT_TOKEN}", methods=["POST"])
+async def webhook():
+    update = Update.de_json(request.get_json(force=True), application.bot)
+    await application.process_update(update)
+    return "ok"
 
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, mesaj_yakala))
+@app_flask.route("/", methods=["GET"])
+def index():
+    return "Bot çalışıyor"
 
-app.run_polling()
+application = ApplicationBuilder().token(BOT_TOKEN).build()
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, mesaj_yakala))
+
+if __name__ == "__main__":
+    app_flask.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
